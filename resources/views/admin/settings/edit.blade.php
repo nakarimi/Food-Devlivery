0ef74cd2c5e293608ef5bfa6a8952f04186da42f
sajadhaibat <sajadhaibat@gmail.com>
2021-02-28 09:40:34 +0430

    width changed for pages

--- resources/views/admin/settings/edit.blade.php
+++ resources/views/admin/settings/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
 
 
-            <div class="col-md-9">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Edit Setting #{{ $setting->id }}</div>
                     <div class="card-body">
