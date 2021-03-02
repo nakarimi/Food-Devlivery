f22b9c441adef1484f5a0f3e0a38f83aafe25f1b
sajadhaibat <sajadhaibat@gmail.com>
2021-03-02 19:21:52 +0430

    retaurant dashboard

---
+++ resources/views/dashboards/restaurant/layouts/master.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('plugins/bootstrap-rtl/css/bootstrap.min.css')}}">
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+
+    <!-- Lineawesome CSS -->
+    <link rel="stylesheet" href="{{asset('css/line-awesome.min.css')}}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{asset('css/RTL-style.css')}}">
+
+    <!-- Custome CSS -->
+    <link rel="stylesheet" href="{{asset('css/custome.css')}}">
+
+
+
+
+    {{--   Adding specific style of each page--}}
+
+    @yield('styles')
+
+</head>
+<body>
+<div id="app">
+    <!-- Main Wrapper -->
+    <div class="main-wrapper">
+
+        <!-- Header -->
+    @include('dashboards.restaurant.layouts.top-header')
+    <!-- /Header -->
+
+        <!-- Sidebar -->
+    @include('dashboards.restaurant.layouts.sidebar')
+    <!-- /Sidebar -->
+
+        <!-- Page Wrapper -->
+        <div class="page-wrapper">
+
+            <!-- Page Content -->
+            <div class="content container-fluid">
+                <!-- Page Header -->
+            {{--                @include('layouts.page-header')--}}
+            <!-- /Page Header -->
+
+
+                @yield('content')
+
+            </div>
+            <!-- /Page Content -->
+
+        </div>
+        <!-- /Page Wrapper -->
+
+    </div>
+    <!-- /Main Wrapper -->
+</div>
+<!-- jQuery -->
+<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+
+<!-- Bootstrap Core JS -->
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('plugins/bootstrap-rtl/js/bootstrap.min.js')}}"></script>
+
+<!-- Slimscroll JS -->
+<script src="{{asset('js/jquery.slimscroll.min.js')}}"></script>
+
+<!-- Custom JS -->
+<script src="{{asset('js/app.js')}}"></script>
+
+<!-- Specific js of pages -->
+@yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/dashboards/restaurant/layouts/sidebar.blade.php
@@ -0,0 +1,26 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title">
+                    <span>Main</span>
+                </li>
+                <li>
+                    <a href="{{url('/')}}"><span>Dashboard</span></a>
+                </li>
+                <li class="submenu">
+                    <a href="#" class="subdrop"><span> Items</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: block;">
+                        <li><a href="{{route('items.pending')}}">Pending Items</a></li>
+                        <li><a href="{{route('items.approved')}}">Approved Items</a></li>
+                    </ul>
+                </li>
+
+
+            </ul>
+
+        </div>
+    </div>
+
+</div>
+
