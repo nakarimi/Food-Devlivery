43c219c5da3c949f1ea0dc9661efec1186e86a43
nasernikzad <n.nikzad@pomtech.com>
2021-03-03 15:46:54 +0430

    Rest API Controllers are created

---
+++ app/Http/Controllers/CustomerRequests.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerRequests extends Controller
+{
+    public function check()
+    {
+       return "Yes this is customer api section.";
+    }
+}
