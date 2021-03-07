498fae862ab103ea4e455a49acb2eca128391a59
nasernikzad <n.nikzad@pomtech.com>
2021-03-07 10:09:40 +0430

    order status updat feature on list

--- public/js/commmon_functions.js
+++ public/js/commmon_functions.js
@@ -4,8 +4,9 @@ Description  : Js codes related to orders.
 */
 
 function show_message(msg) {
-    $('div.card-body').prepend('<div id="success-alert" class="alert alert-success alert-dismissible fade show" role="alert"><strong>'+msg+'</strong></div>');
+    $('div.card-body').prepend('<div style="position: fixed;bottom: 0;right: 0;" id="success-alert" class="alert alert-success alert-dismissible fade show" role="alert"><strong>'+msg+'</strong></div>');
     $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-        $("#success-alert").slideUp(500);
+        $("#success-alert").slideUp(2500);
     });
+
 }
