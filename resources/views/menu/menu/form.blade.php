cd23e827e129a06da464ae98eff9e067ce8dc444
Dev Site For Food Delivery Project <dev.food@pomdev.net>
2021-03-08 13:56:33 +0430

    menu fix bug

--- resources/views/menu/menu/form.blade.php
+++ resources/views/menu/menu/form.blade.php
@@ -20,14 +20,28 @@
 </div>
 
 <div class="form-group{{ $errors->has('branch_id') ? 'has-error' : ''}}">
+    
+    
+    @if(Auth::user()->role->name == "restaurant")
+    <div style="display: none;">
+        <label for="branch_id" class="control-label">{{ 'Branch Id' }}</label>
+        <select class="custom-select mr-sm-2" name="branch_id" id="branch_id" required>
+            @foreach($branches as $branch)
+                <option value="{{ $branch->id }}" selected="selected" >{{ $branch->branchDetails->title }}</option>
+            @endforeach
+        </select>
+    </div>
+    @else
     <label for="branch_id" class="control-label">{{ 'Branch Id' }}</label>
-    {{-- <input class="form-control" name="branch_id" type="number" id="branch_id" value="{{ $item->branch_id ?? ''}}" required> --}}
     <select class="custom-select mr-sm-2" name="branch_id" id="branch_id" required>
         <option  value="" selected disabled>Select Branch</option>
         @foreach($branches as $branch)
             <option value="{{ $branch->id }}" @if( isset($item->branch_id) && $branch->id == $item->branch_id) selected="selected" @endif >{{ $branch->branchDetails->title }}</option>
         @endforeach
     </select>
+    @endif
+    
+    
     {!! $errors->first('branch_id', '
     <p class="help-block">:message</p>
     ') !!}
