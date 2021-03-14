433d43444abc379dd6402a35e0f6891d37b647cf
Dev Site For Food Delivery Project <dev.food@pomdev.net>
2021-03-14 16:33:31 +0430

    seed updated with a new order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -109,6 +109,14 @@ class DatabaseSeeder extends Seeder
                 'role_id' => 5,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ), 
+            array (
+                'name'     => 'Ghulam Bargar',
+                'email'    => 'g.long@bargar.com',
+                'password' => bcrypt('asdfasdf'),
+                'role_id' => 4,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             )
 
         ]);
@@ -120,26 +128,33 @@ class DatabaseSeeder extends Seeder
 
         DB::table('branches')->insert(array(
             array('id' => '1','created_at' => NULL,'updated_at' => NULL,'user_id' => '4','business_type' => 'food','main_commission_id' => '1','deliver_commission_id' => '2','status' => '1'),
-            array('id' => '2','created_at' => NULL,'updated_at' => NULL,'user_id' => '5','business_type' => 'food','main_commission_id' => '1','deliver_commission_id' => NULL,'status' => '1')
+            array('id' => '2','created_at' => NULL,'updated_at' => NULL,'user_id' => '5','business_type' => 'food','main_commission_id' => '1','deliver_commission_id' => NULL,'status' => '1'),
+            array('id' => '3','created_at' => NULL,'updated_at' => NULL,'user_id' => '8','business_type' => 'food','main_commission_id' => '1','deliver_commission_id' => NULL,'status' => '1')
         ));
 
         DB::table('branche_main_info')->insert(array(
             array('id' => '1','created_at' => NULL,'updated_at' => NULL,'business_id' => '1','title' => 'Fifty-Fifty','description' => 'Eat anything with 50 Afs.','logo' => 'test_1614666474.png','contact' => '+937909090','location' => 'Herat-Chawk Golha','status' => 'approved'),
-            array('id' => '2','created_at' => NULL,'updated_at' => NULL,'business_id' => '2','title' => 'Herat Super Market','description' => 'Any you need, we have.','logo' => 'logo_1614666570.png','contact' => '+937303030','location' => 'Herat-Chawk Shahre Now','status' => 'approved')
+            array('id' => '2','created_at' => NULL,'updated_at' => NULL,'business_id' => '2','title' => 'Herat Super Market','description' => 'Any you need, we have.','logo' => 'logo_1614666570.png','contact' => '+937303030','location' => 'Herat-Chawk Shahre Now','status' => 'approved'),
+            array('id' => '3','created_at' => NULL,'updated_at' => NULL,'business_id' => '3','title' => 'Ghulam Bargar','description' => 'Any you need, we have.','logo' => 'logo_1614666570.png','contact' => '+937303030','location' => 'Herat-Chawk Shahre Now','status' => 'approved')
         ));
 
         DB::table('items')->insert(array(
             array('id' => '1','created_at' => NULL,'updated_at' => NULL,'branch_id' => '1','category_id' => '1', 'status' => '1'),
             array('id' => '2','created_at' => NULL,'updated_at' => NULL,'branch_id' => '1','category_id' => '1', 'status' => '1'),
             array('id' => '3','created_at' => NULL,'updated_at' => NULL,'branch_id' => '2','category_id' => '2','status' => '1'),
-            array('id' => '4','created_at' => NULL,'updated_at' => NULL,'branch_id' => '2','category_id' => '2','status' => '1')
+            array('id' => '4','created_at' => NULL,'updated_at' => NULL,'branch_id' => '2','category_id' => '2','status' => '1'),
+             array('id' => '5','created_at' => NULL,'updated_at' => NULL,'branch_id' => '3','category_id' => '2','status' => '1'),
+              array('id' => '6','created_at' => NULL,'updated_at' => NULL,'branch_id' => '3','category_id' => '2','status' => '1')
+
           ));
 
         DB::table('item_details')->insert(array(
             array('id' => '1','created_at' => NULL,'updated_at' => NULL,'item_id' => '1','title' => 'Pizza','description' => 'پیتزا مخصوص با سوس','thumbnail' => 'noimage.jpg','price' => '50','package_price' => '0','unit' => 'piece','details_status' => 'approved'),
             array('id' => '2','created_at' => NULL,'updated_at' => NULL,'item_id' => '2','title' => 'همبرگر','description' => 'همبرگر با نون اضافی','thumbnail' => 'noimage.jpg','price' => '70','package_price' => '10','unit' => 'piece','details_status' => 'approved'),
             array('id' => '3','created_at' => NULL,'updated_at' => NULL,'item_id' => '3','title' => 'روغن','description' => '1 کیلو','thumbnail' => 'noimage.jpg','price' => '120','package_price' => '0','unit' => 'bottle','details_status' => 'approved'),
-            array('id' => '4','created_at' => NULL,'updated_at' => NULL,'item_id' => '4','title' => 'کچالو','description' => 'کچالوی غزنی','thumbnail' => 'noimage.jpg','price' => '25','package_price' => '0','unit' => 'کیلو','details_status' => 'approved')
+            array('id' => '4','created_at' => NULL,'updated_at' => NULL,'item_id' => '4','title' => 'کچالو','description' => 'کچالوی غزنی','thumbnail' => 'noimage.jpg','price' => '25','package_price' => '0','unit' => 'کیلو','details_status' => 'approved'),
+             array('id' => '5','created_at' => NULL,'updated_at' => NULL,'item_id' => '5','title' => 'شوربا','description' => 'کچالوی غزنی','thumbnail' => 'noimage.jpg','price' => '25','package_price' => '0','unit' => 'کیلو','details_status' => 'approved'),
+              array('id' => '6','created_at' => NULL,'updated_at' => NULL,'item_id' => '6','title' => 'دیزی','description' => 'کچالوی غزنی','thumbnail' => 'noimage.jpg','price' => '25','package_price' => '0','unit' => 'کیلو','details_status' => 'approved')
           ));
 
         DB::table('categories')->insert(array(
@@ -156,7 +171,8 @@ class DatabaseSeeder extends Seeder
             array('id' => '2','created_at' => '2021-03-04 11:12:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'Second new order','branch_id' => '1','customer_id' => '6','has_delivery' => '0','total' => '200','commission_value' => '20','status' => 'approved','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "1"}}, {"item_2": {"count": "4", "price": "100", "item_id": "2"}}, {"item_3": {"count": "8", "price": "200", "item_id": "5"}}]}'),
             array('id' => '3','created_at' => '2021-03-04 2:02:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'Third new order','branch_id' => '1','customer_id' => '6','has_delivery' => '1','total' => '200','commission_value' => '20','status' => 'reject','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "1"}}, {"item_2": {"count": "4", "price": "100", "item_id": "2"}}, {"item_3": {"count": "8", "price": "200", "item_id": "5"}}]}'),
             array('id' => '4','created_at' => '2021-03-04 10:25:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'Fourth new order','branch_id' => '1','customer_id' => '6','has_delivery' => '0','total' => '200','commission_value' => '20','status' => 'processing','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "1"}}, {"item_2": {"count": "4", "price": "100", "item_id": "2"}}, {"item_3": {"count": "8", "price": "200", "item_id": "5"}}]}'),
-            array('id' => '5','created_at' => '2021-03-04 10:00:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'Fifth order','branch_id' => '1','customer_id' => '6','has_delivery' => '0','total' => '200','commission_value' => '20','status' => 'completed','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "1"}}, {"item_2": {"count": "4", "price": "100", "item_id": "2"}}, {"item_3": {"count": "8", "price": "200", "item_id": "5"}}]}')
+            array('id' => '5','created_at' => '2021-03-04 10:00:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'Fifth order','branch_id' => '1','customer_id' => '6','has_delivery' => '0','total' => '200','commission_value' => '20','status' => 'completed','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "1"}}, {"item_2": {"count": "4", "price": "100", "item_id": "2"}}, {"item_3": {"count": "8", "price": "200", "item_id": "5"}}]}'),
+            array('id' => '6','created_at' => '2021-03-04 10:00:15','updated_at' => NULL,'deleted_at' => NULL,'title' => 'test order by PM','branch_id' => '3','customer_id' => '6','has_delivery' => '0','total' => '200','commission_value' => '10','status' => 'completed','note' => 'test node','reciever_phone' => '0790909090','contents' => '{"contents": [{"item_1": {"count": "2", "price": "100", "item_id": "6"}}]}')
         ));
 
         DB::table('order_timing')->insert(array(
@@ -164,7 +180,8 @@ class DatabaseSeeder extends Seeder
             array('id' => '2','created_at' => NULL,'updated_at' => NULL,'order_id' => '2','approved_time' => '2021-03-04 05:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL),
             array('id' => '3','created_at' => NULL,'updated_at' => NULL,'order_id' => '3','approved_time' => '2021-03-04 10:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL),
             array('id' => '4','created_at' => NULL,'updated_at' => NULL,'order_id' => '4','approved_time' => '2021-03-04 11:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL),
-            array('id' => '5','created_at' => NULL,'updated_at' => NULL,'order_id' => '5','approved_time' => '2021-03-04 10:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL)            
+            array('id' => '5','created_at' => NULL,'updated_at' => NULL,'order_id' => '5','approved_time' => '2021-03-04 10:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL),
+             array('id' => '6','created_at' => NULL,'updated_at' => NULL,'order_id' => '6','approved_time' => '2021-03-04 10:52:15','rejected_time' => NULL,'processing_time' => NULL,'caceled_time' => NULL,'delivery_time' => NULL,'completed_time' => NULL)            
         ));
 
         DB::table('order_delivery')->insert(array(
