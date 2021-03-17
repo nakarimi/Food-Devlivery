b3757156aa48d6467a47d4bd7f6fdc9499fda37b
nasernikzad <n.nikzad@pomtech.com>
2021-03-17 14:07:23 +0430

    unneeded files are removed

--- resources/views/Alerts/message-alert.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="alert {{ session()->get('alertType') }} alert-dismissible" id="message-alert">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    {{ session()->get('message') }}
-</div>
-
