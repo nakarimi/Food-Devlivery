d2b9d6adbb35e821cee5a228078c5891b22bd10c
nasernikzad <n.nikzad@pomtech.com>
2021-04-20 08:01:15 +0430

    favorite restauramts added

---
+++ database/migrations/2021_04_20_075801_favorite_restaurants.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FavoriteRestaurants extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorited_restaurant', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('customer_id');
+            $table->integer('branch_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('favorited_restaurant');
+    }
+}
